91bc3d4d66ba099a8fa85321bb1a82f515e72da6
KatheVega <kathevega03@gmail.com>
2023-11-11 07:49:40 -0500

    Commit Taller

---
+++ BackendMascotaFeliz/src/services/autenticacion.service.ts
@@ -0,0 +1,68 @@
+import {injectable, /* inject, */ BindingScope} from '@loopback/core';
+import { repository } from '@loopback/repository';
+import {UsuarioRepository} from '../repositories';
+import {Llaves} from '../config/llaves'
+import { Usuario } from '../models';
+const generador =require('password-generator');
+const cryptoJS = require('crypto-js');
+const jwt = require('jsonwebtoken')
+
+// solo son advertensias
+@injectable({scope: BindingScope.TRANSIENT})
+export class AutenticacionService {
+  constructor(
+    @repository(UsuarioRepository)
+    public usuarioRepositorio:UsuarioRepository
+  ) {}
+
+  /*
+   * Add service methods here
+   */
+
+  GenerarClave(){
+    let clave = generador(8,false);
+    return clave;
+  }
+
+  CifrarClave( clave: string){
+    let claveCifrada = cryptoJS.MD5(clave).toString();
+    return claveCifrada;
+  }
+
+  IdentificarUsuario(usuario:string, clave:string){
+    try {
+      let p = this.usuarioRepositorio.findOne({where:{correo:usuario, contrasena:clave }});
+      if (p){
+        return p;
+      }
+      return false;
+    } catch {
+      return false;
+    }
+  }
+
+  GenerarTokenJWT(usuario:Usuario){
+    let token = jwt.sign({
+      data:{
+        id: usuario.id,
+        correo: usuario.correo,
+        nombre: usuario.nombre,
+        rol: usuario.rol,
+        apellido: usuario.apellido
+      }
+    },
+    Llaves.claveJWT
+    );
+    return  token;
+  }
+
+  ValidarTokenJWT(token:string){
+    try {
+      let datos = jwt.verify(token, Llaves.claveJWT);
+      return datos;
+    } catch {
+      return false;
+    }
+  }
+
+}
